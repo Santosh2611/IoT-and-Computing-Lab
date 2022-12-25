aa03512199e99ed9fcf09873ff9ea0a7cb59a710
Santosh <88974296+Santosh2611@users.noreply.github.com>
2022-12-25 13:47:45 +0530

    Create 19CCE381_Expt_6_Prerequisites.py

---
+++ Experiment 6 - Image Processing using Raspberry Pi/19CCE381_Expt_6_Prerequisites.py
@@ -0,0 +1,6 @@
+# Install Pillow library using the following command - sudo pip3 install pillow
+# Install an image viewing utility called xv for the built-in function show() to work.
+#     sudo apt-get install xli-y
+#     cd /usr/local/bin
+#     sudo ln -s /usr/bin/xli xv
+# Import Tkinter library for GUI support in Python - sudo apt-get install python3-pil.imagetk
