0439faa267b1ab2a16f28cbf3c7b02156fd386bc
Santosh <88974296+Santosh2611@users.noreply.github.com>
2022-12-25 13:44:47 +0530

    Create Libraries_Installation_Version.py

---
+++ Experiment 5 - Machine Learning using IRIS Dataset/Libraries_Installation_Version.py
@@ -0,0 +1,18 @@
+import sys # Provides access to some variables used or maintained by the interpreter and to functions that interact strongly with the interpreter
+print('Python: {}'.format(sys.version))
+
+import scipy # Includes modules for statistics, optimization, integration, linear algebra, Fourier transforms, signal and image processing, and more
+print('scipy: {}'.format(scipy.__version__))
+
+import numpy # Support for large, multi-dimensional arrays and matrices
+print('numpy: {}'.format(numpy.__version__))
+
+import matplotlib # Provides a MATLAB-like plotting framework
+print('matplotlib: {}'.format(matplotlib.__version__))
+
+import pandas # Library for working with data sets
+print('pandas: {}'.format(pandas.__version__))
+
+import sklearn # Provides various tools for model fitting, data preprocessing, model selection, model evaluation, and many other utilities
+print('sklearn: {}'.format(sklearn.__version__))
+
